1ef448d8d0c50a5e47610c41e8b60d91d70cbbc1
Pavel Stelmakh <pavelstelmax@rambler.ru>
2019-07-29 00:30:53 +0300

    did template for request

--- config.js
+++ config.js
@@ -4,3 +4,4 @@ dotenv.config();
 
 export const PORT = process.env.PORT || 8080;
 export const NODE_ENV = process.env.NODE_ENV || 'development';
+export const HOST = process.env.HOST || 'localhost';
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import graphql from 'graphql';
+import { HOST, PORT } from './config';
+
+const DOMAIN = `${HOST}:${PORT}` 
 
 const getUsersButton = document.querySelector('.users');
 const getUserButton = document.querySelector('.user-by-id');
@@ -6,6 +8,18 @@ const createUserButton = document.querySelector('.create-user');
 const createActicleButton = document.querySelector('.create-article');
 
 const resultBlock = document.querySelector('.result-block');
+
+const graphql = (query, variables) => fetch(DOMAIN, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    },
+    body: JSON.stringify({
+        query,
+        variables
+    }),
+}).then(r => r.json());
 
 getUsersButton.addEventListener('click', () => {
     resultBlock.innerText = 'fdf';
