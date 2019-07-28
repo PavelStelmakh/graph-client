eea8280a055adfef1ef6562d834a13e21d040cc7
Pavel Stelmakh <pavelstelmax@rambler.ru>
2019-07-29 00:12:22 +0300

    init webpack babel

---
+++ index.js
@@ -0,0 +1,24 @@
+import graphql from 'graphql';
+
+const getUsersButton = document.querySelector('.users');
+const getUserButton = document.querySelector('.user-by-id');
+const createUserButton = document.querySelector('.create-user');
+const createActicleButton = document.querySelector('.create-article');
+
+const resultBlock = document.querySelector('.result-block');
+
+getUsersButton.addEventListener('click', () => {
+    resultBlock.innerText = 'fdf';
+});
+
+getUserButton.addEventListener('click', () => {
+    
+});
+
+createUserButton.addEventListener('click', () => {
+    
+});
+
+createActicleButton.addEventListener('click', () => {
+    
+});
